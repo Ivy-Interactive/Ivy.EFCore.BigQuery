4d441cf818d80a56e68799006d4c99c99240507e
Dovydas Meilūnas <dovydas.m@gmail.com>
2025-05-13 07:09:30 +0300

    Env variable for connection string, test instructions

--- tests/Ivy.EFCore.BigQuery.Conformance.Tests/DbFactoryFixture.cs
+++ tests/Ivy.EFCore.BigQuery.Conformance.Tests/DbFactoryFixture.cs
@@ -7,8 +7,8 @@ namespace Ivy.EFCore.BigQuery.Data.Conformance.Tests
     {
         public DbFactoryFixture()
         {
-            //Todo environment variable
-            ConnectionString = "DataSource=http://localhost:9050;AuthMethod=ApplicationDefaultCredentials;ProjectId=test;DefaultDatasetId=ado_tests";
+            ConnectionString = Environment.GetEnvironmentVariable("BQ_ADO_CONN_STRING") ??
+                               "DataSource=http://localhost:9050;AuthMethod=ApplicationDefaultCredentials;ProjectId=test;DefaultDatasetId=ado_tests";
         }
 
         //public string CreateBooleanLiteral(bool value)
