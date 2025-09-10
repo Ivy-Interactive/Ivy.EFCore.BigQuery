cdb849507e351ed96da4d9425deddd30d0d4cbc1
Dovydas Meilūnas <dovydas.m@gmail.com>
2025-09-10 12:21:26 +0300

    ConventionSetBuilder

---
+++ src/Ivy.EFCore.BigQuery/Metadata/Conventions/BigQueryConventionSetBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Ivy.EFCore.BigQuery.Metadata.Conventions;
+
+public class BigQueryConventionSetBuilder : RelationalConventionSetBuilder
+{
+    private readonly IRelationalTypeMappingSource _typeMappingSource;
+
+    public BigQueryConventionSetBuilder(
+        ProviderConventionSetBuilderDependencies dependencies,
+        RelationalConventionSetBuilderDependencies relationalDependencies,
+        IRelationalTypeMappingSource typeMappingSource)
+        : base(dependencies, relationalDependencies)
+    {
+        _typeMappingSource = typeMappingSource;
+
+    }
+
+    public override ConventionSet CreateConventionSet()
+    {
+        var conventionSet = base.CreateConventionSet();
+
+        return conventionSet;
+    }
+}
